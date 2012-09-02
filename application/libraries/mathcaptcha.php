b4051ff65b4bb6cb736b2c7c35d2812865161a33
Dan Murfitt <dan@murfitt.net>
2012-09-02 16:03:42 +0100

    Added mathcaptcha library.

---
+++ application/libraries/mathcaptcha.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+Class Mathcaptcha
+{
+    private $ci;
+    
+    public function __construct() {
+        //Get the CodeIgniter super object
+        $this->ci =& get_instance();
+    }
+    
+    
+}
+
+/* End of file mathcaptcha.php */
+/* Location: ./application/libraries/mathcaptcha.php */
